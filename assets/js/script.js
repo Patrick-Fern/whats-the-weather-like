2c33914d965b8deacaf26520d6fb77fc53539469
patfern <patrick.j.fernbach@gmail.com>
2022-01-29 14:27:41 -0500

    city button persistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
 var citySearchButtonEl = document.querySelector("#city-search-button");
 var cityInputEl = document.querySelector("#search-city");
 var cityListEl = document.querySelector("#cities-list")
+var cities = []
 
+var saveCities = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
 
+var loadCities = function() {
+    cities = JSON.parse(localStorage.getItem("cities"));
+
+    if (cities === null) {
+        cities = [];
+        return
+    }  else {
+        for (var i = 0; i < cities.length; i++) {
+           var cityName = cities[i]
+           var cityEl = document.createElement("button");
+        cityEl.classList = "btn btn-secondary"
+    
+        cityEl.textContent = cityName
+        cityListEl.appendChild(cityEl);
+
+        }
+    }
+}
+
+loadCities();
 
 var addCity = function(event) {
     event.preventDefault();
@@ -11,12 +35,12 @@ var addCity = function(event) {
 
     var cityEl = document.createElement("button");
     cityEl.classList = "btn btn-secondary"
+    //cityEl.setAttribute("data-cityName", cityName)
     cityEl.textContent = cityName
 
     cityListEl.appendChild(cityEl);
-
-
-
+    cities.push(cityName);
+    saveCities();
 };
 
 
