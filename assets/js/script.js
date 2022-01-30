eef97912e05859d90729d589069e1a1876a595a9
patfern <patrick.j.fernbach@gmail.com>
2022-01-29 22:59:09 -0500

    fetch and display today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 var citySearchButtonEl = document.querySelector("#city-search-button");
 var cityInputEl = document.querySelector("#search-city");
-var cityListEl = document.querySelector("#cities-list")
+var cityListEl = document.querySelector("#cities-list");
+var searchedCityEl = document.querySelector("#searched-city");
 var cities = []
+var todayTempEl = document.querySelector("#today-temp");
+var todayWindEl = document.querySelector("#today-wind");
+var todayHumidityEl = document.querySelector("#today-humidity");
+var todayUvEl = document.querySelector("#today-uv"); 
+
+var getTodayWeather = function(city) {
+    searchedCityEl.innerHTML = city;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ee0df90445baa29f925de9ef4662322f"
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data){
+            console.log(data);
+            todayTempEl.innerHTML = "Temperature: " + data.main.temp + "*F";
+            todayWindEl.innerHTML = "Wind: " + data.wind.speed + " MPH";
+            todayHumidityEl.innerHTML = "Humidity: " + data.main.humidity + "%";
+            todayUvEl.innerHTML = "UV Index: "  
+        })
+    });
+};
+
+
+
 
 var saveCities = function () {
     localStorage.setItem("cities", JSON.stringify(cities));
@@ -24,7 +47,7 @@ var loadCities = function() {
 
         }
     }
-}
+};
 
 loadCities();
 
@@ -41,6 +64,9 @@ var addCity = function(event) {
     cityListEl.appendChild(cityEl);
     cities.push(cityName);
     saveCities();
+
+    getTodayWeather(cityName);
+    
 };
 
 
